b9806cafe7157f53e79e1f6cdbe112b2165d281a
Nicholas DuFour <nick.dufour48@gmail.com>
2023-04-15 02:23:17 -0400

    working on routes

--- seeds/index.js
+++ seeds/index.js
@@ -2,12 +2,11 @@ const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
-
 const sequelize = require('../config/connection');
-
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
+  
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
@@ -22,5 +21,4 @@ const seedAll = async () => {
 
   process.exit(0);
 };
-
 seedAll();
